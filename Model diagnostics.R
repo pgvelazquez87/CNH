3e4c4aa33f329a8e7f3a2d9e9401cc4068581eaa
Pablo Gerardo Velázquez Villa <pablo.velazquez@cnh.gob.mx>
2018-10-12 16:25:33 -0500

    Cambios model diagnostics

--- Model diagnostics.R
+++ Model diagnostics.R
@@ -1,18 +1,132 @@
 ##MODEL DIAGNOSTICS
 
 library(boot)
+library(ggplot2)
+library(coefplot)
+library(reshape2)
 
 housing <- read.table("http://www.jaredlander.com/data/housing.csv", sep=",", header=TRUE, stringsAsFactors = FALSE)
 names(housing) <- c("Neighborhood", "Class", "Units", "YearBuilt", "SqFt", "Income", "IncomePerSqFt", "Expense", "ExpensePerSqFt", "NetIncome", "Value", "ValuePerSqFt", "Boro")
+housing <- housing[housing$Units<1000, ]
 
+house1 <- lm(ValuePerSqFt ~ Units + SqFt + Boro, data=housing)
 houseg1<- glm(ValuePerSqFt ~ Units + SqFt + Boro, data=housing, family=gaussian(link = "identity"))
 houseg2 <- glm(ValuePerSqFt ~ Units * SqFt + Boro, data=housing)
 houseg3 <- glm(ValuePerSqFt ~ Units + SqFt*Boro + Class, data=housing)
-houseg4 <-  glm(ValuePerSqFt ~ Units + SqFt * Boro + SqFt*Class, data=housing)
+houseg4 <-  glm(ValuePerSqFt ~ Units + SqFt*Boro + SqFt*Class, data=housing)
 houseg5 <- glm(ValuePerSqFt ~ Boro + Class, data=housing)
 
-obj <- list("2"=houseg2, "3"=houseg3)
 
-for (i in 2:3)  {
-  assign(paste0("housecv", i), cv.glm(housing, obj[["i"]] , K=5))
+for (i in 1:5)  {
+  nam <- paste0("houseg", i)
+  print(nam)
+  assign(paste0("housecv", i), cv.glm(housing, get(nam), K=5))
+  print(get(paste0("housecv", i))$delta)
 }
+
+cvresults <- as.data.frame(rbind(housecv1$delta, housecv2$delta, housecv3$delta, housecv4$delta, housecv5$delta))
+names(cvresults) <- c("Error", "Adjusted.Error")
+cvresults$model <- sprintf("houseg%s", 1:5)
+
+#Comparar los distintos modelos a través de distintos mecanismos
+cvanova <- anova(houseg1, houseg2, houseg3, houseg4, houseg5)
+cvresults$anova <- cvanova$`Resid. Dev`
+cvaic <- AIC(houseg1, houseg2, houseg3, houseg4, houseg5)
+cvresults$aic <- cvaic$AIC
+
+cvmelt <- melt(cvresults, id.vars="model", variable.name="measure", value.name=)
+ggplot(aes(x=model, y=value), data=cvmelt) + geom_line(aes(group=measure, color=measure)) + facet_wrap(~measure, scales="free_y") + theme(axis.text.x=element_text(angle=90, vjust=0.5)) + guides(color=FALSE)
+
+
+
+################################################################################
+#### modelo general para cross-validation
+
+cv.work <- function(fun, k=5, data, cost = function(y, yhat) mean ((y-yhat)^2), response = "y", ...)
+{
+  ##generate folds
+  folds <- data.frame(fold=sample(rep(x=1:5, length.out=nrow(housing))), row=1:nrow(housing))
+  
+  ##start the error with 0
+  error <- 0
+  
+  ##loop through each of the folds
+  ## for each fold:
+  ## fit the model on the training data
+  ## predict on the test data
+  ## compute the error and accumulate it
+  for(f in 1:max(folds$fold))
+  {
+    # rows that are in test set
+    therows <- folds$row[folds$fold == f]
+    
+    # call fun on data[-therows, ]
+    # predict on data[therows, ]
+    mod <- fun(data=data[-therows, ], ... )
+    pred <- predict(mod, data[therows, ])
+    
+    # add new error weighted by the number of rows in this fold
+    error <- error + cost(data[therows, response], pred) * (length(therows)/nrow(data))
+  }
+  
+  return(error)
+}
+
+##Correr los modelos con la función
+cv1 <- cv.work(fun=lm, k=5, data=housing, response="ValuePerSqFt", formula=ValuePerSqFt ~ Units + SqFt + Boro)
+cv2 <- cv.work(fun=lm, k=5, data=housing, response="ValuePerSqFt", formula=ValuePerSqFt ~ Units * SqFt + Boro)
+cv3 <- cv.work(fun=lm, k=5, data=housing, response="ValuePerSqFt", formula=ValuePerSqFt ~ Units + SqFt*Boro + Class)
+cv4 <- cv.work(fun=lm, k=5, data=housing, response="ValuePerSqFt", formula=ValuePerSqFt ~ Units + SqFt*Boro + SqFt*Class)
+cv5 <- cv.work(fun=lm, k=5, data=housing, response="ValuePerSqFt", formula=ValuePerSqFt ~ Boro + Class)
+
+cvResults <- data.frame(model=sprintf("house%s", 1:5), error=c(cv1, cv2, cv3, cv4, cv5))
+
+
+
+
+
+#############################
+## BOOTSTRAPPING
+#############################
+
+library(plyr)
+library(boot)
+library(ggplot2)
+
+data(baseball)
+baseball <- baseball[baseball$year>=1990, ]
+head(baseball)
+
+#write.csv(baseball, file="baseball.csv")
+#baseball$battingavg[is.infinite(baseball$battingavg)] <- NaN
+#summary(baseball, na.rm=TRUE)
+
+##build a function for calculating batting average
+#data is the data
+#boot will pass varying sets of indices
+#some rows will be represented multiple times in a single pass
+#other rows will not be represented at all
+#on average, about 63% of the rows will be present
+#this function is called repeatedly by boot
+
+
+bat.avg <- function(data, indices=1:NROW(data), hits="h", at.bats="ab")
+{
+ sum(data[indices, hits], na.rm=TRUE) / sum(data[indices, at.bats], na.rm=TRUE)
+}
+
+bat.avg(baseball)
+
+##bootstrap it
+#using the baseball data, call bat.avg 1,200 times
+#pass indices to the function
+avgboot <- boot(data=baseball, statistic = bat.avg, R=1200, stype="i")
+
+#print original measure and estimates of bias and standard error
+avgboot
+
+#print the confidence interval
+boot.ci(avgboot, conf=0.95, type="norm")
+
+#graph
+ggplot() + geom_histogram(aes(x=avgboot$t), fill='grey', color="grey") + geom_vline()
--- Rgasolina.R
+++ Rgasolina.R
@@ -1,8 +1,20 @@
+###########################################################
+# Análisis de información de demanda de gasolinas         #
+# Dirección General de Estadística y Evaluación Económica #
+###########################################################
+
+
 #install.packages("dplyr")
 #install.packages("tidyr")
 #install.packages("reshape")
 #install.packages("ggplot2")
 #install.packages("qdapRegex")
+#install.packages("ggmap")
+#install.packages("stringr")
+#install.packages("maps", dependencies=TRUE)
+#install.packages("mapdata", dependencies=TRUE)
+#install.packages("stringi", type="win.binary")
+#install.packages("lubridate")
 
 library(plyr)
 library(dplyr)
@@ -11,11 +23,29 @@ library(reshape)
 library(ggplot2)
 library(readxl)
 library(qdapRegex)
+library(devtools)
+library(rjson)
+library(scales)
+library(ggmap)
+library(lattice)
+library(stringr)
+library(maps)
+library(ggthemes)
+library(mapdata)
+library(data.table)
+library(gganimate)
+library(tibble)
+library(lubridate)
 
 setwd("G:/Proyectos/Prospectiva 2018-2032/Prospectiva/Gasolina")
 
-## deshabilitar notaci?n cient?fica
+## deshabilitar notación científica
 options(scipen = 999)
+
+
+####################################
+# Cargar la informacion de oferta #
+####################################
 
 ##funcion
 agregado <- function(data)
@@ -28,11 +58,12 @@ w_aceite <- read.csv("proceso_crudo.csv")
 names(w_aceite)[1] <- "Refineria"
 p_aceite <- melt(w_aceite, id=(c("Refineria", "Crudo")))
 p_aceite$Crudo <- trimws(p_aceite$Crudo, which = c("left"))
-p_aceite$Crudo <- gsub("?", "i", p_aceite$Crudo)
+p_aceite$Crudo <- gsub("Ã", "i", p_aceite$Crudo)
 p_aceite$Crudo <- gsub("Otras corrientes a", "Otro", p_aceite$Crudo)
 p_aceite$Crudo <- gsub("Reconstituido", "Pesado", p_aceite$Crudo)
 p_aceite$Crudo <- as.factor(p_aceite$Crudo)
 
+#¿Qué porcentaje de tipo de crudo surte a cada refinería?
 tipo.crudo <- ddply(p_aceite, .(Refineria, Crudo), .fun=agregado)
 total.crudo <- ddply(p_aceite, .(Refineria), .fun=agregado)
 total1 <- merge(tipo.crudo, total.crudo, by=c("Refineria"))
@@ -40,7 +71,8 @@ total1$pct <- total1$pct.x / total1$pct.y
 total1 <- subset(total1, select = -c(pct.x, pct.y))
 
 
-w_producto <- read.csv("producto_petrolifero.csv")
+# ¿Qué petrolíferos son los que se producen en cada refinería?
+w_producto <- read.csv("produccion_petrolifero.csv")
 names(w_producto)[1] <- "Refineria"
 p_producto <- melt(w_producto, id=(c("Refineria", "Producto")))
 p_producto$Producto <- gsub("Ã³", "o", p_producto$Producto)
@@ -50,49 +82,47 @@ p_producto$Producto <- gsub("Gasoleo industrial d", "Otro", p_producto$Producto)
 p_producto$Producto <- gsub("Nova/Base c", "Otro", p_producto$Producto)
 p_producto$Producto <- gsub("Lubricantes", "Otro", p_producto$Producto)
 p_producto$Producto <- gsub("Otros e", "Otro", p_producto$Producto)
-p_producto$Producto <- gsub("            Extra/Pemex Magna b", "Magna", p_producto$Producto)
+p_producto$Producto <- gsub("Pemex Premium", "Gasolinas", p_producto$Producto)
+p_producto$Producto <- gsub("            Extra/Pemex Magna b", "Gasolinas", p_producto$Producto)
 p_producto$Producto <- gsub("Gas Seco a", "Gas seco", p_producto$Producto)
 p_producto$Producto <- trimws(p_producto$Producto, which = c("left"))
 p_producto$Producto <- as.factor(p_producto$Producto)
+p_producto$variable <- ymd(sprintf(paste0(str_replace(string=p_producto$variable, pattern = "\\w(\\d)", replacement = "\\1"),"-01-01"), p_producto$variable))
+p_producto<- p_producto %>% filter(Producto %in% c('Gasolinas', 'Turbosina', 'Diesel'))  %>% filter(variable<'2018-01-01') %>% group_by(Refineria, Producto, variable) %>% summarize(value = sum(value))
 
 
+
+# ¿Cuánto produce de cada producto cada Refínería?
 tipo.producto <- ddply(p_producto, .(Refineria, Producto), .fun=agregado)
 total.producto <- ddply(p_producto, .(Refineria), .fun=agregado)
 total2 <- merge(tipo.producto, total.producto, by=c("Refineria"))
 total2$pct <- total2$pct.x / total2$pct.y
 total2 <- subset(total2, select = -c(pct.x, pct.y))
 
-
+#¿Qué porcentaje de crudo en qué refinería produce qué petrolífero?
 tot <- merge(total1, total2, by=c("Refineria"))
 tot$value <- (tot$pct.x * tot$pct.y)*100
 tot <- ddply(tot, .(Refineria, Crudo, Producto), .fun=agregado)
 
-cap <- c(275,177,285,220,330,315)
-Refineria <- c("Cadereyta", "Madero", "Minatitlan", "Salamanca", "Salina Cruz", "Tula")
-capacidad <- data.frame(Refineria, cap)
 
+cap <- c(275,177,285,220,330,315)
+lon <- c(-99.942398, -97.81048, -94.460094, -101.178782, -95.171192, -99.269776)
+lat <- c(25.586819, 22.265767, 17.999697, 20.581475, 16.209, 20.049787)
+Refineria <- c("Cadereyta", "Madero", "Minatitlan", "Salamanca", "Salina Cruz", "Tula")
+capacidad <- data.frame(Refineria, cap, lon, lat)
+p_producto <- merge(p_producto, capacidad, by='Refineria')
+
+#¿Cuál es la capacidad máxima de producción de gasolinas por refinería y tipo de crudo?
 tot <- merge(tot, capacidad, by=c("Refineria"))
 tot$value <- (tot$pct * tot$cap)/100
-
 ddply(tot, .(Crudo, Producto), .fun=agregado)
-write.csv(tot, file="tot.csv")
 
 
-#install.packages("ggmap")
-#install.packages("stringr")
-#install.packages("maps", dependencies=TRUE)
-#install.packages("mapdata", dependencies=TRUE)
-library(devtools)
-library(rjson)
-library(ggmap)
-library(lattice)
-library(stringr)
-library(maps)
-library(mapdata)
-library(data.table)
 
+####################################
+# Cargar la informacion de demanda #
+####################################
 
-## Cargar la informaci?n de demanda
 demanda_gas <- read_excel('gasolina_entidad.xlsx', sheet='Base')
 demanda_gas$estado <- rm_between(demanda_gas$Superintendencia, "(", ")", extract=TRUE)
 demanda_gas$Petrolifero <- as.factor(demanda_gas$Petrolifero)
@@ -113,7 +143,7 @@ demanda_gas3 <- data.frame(demanda_gas3[ , ], geocodes)
 
 # gráficar la información
 mex <- map_data("worldHires", "Mexico")
-gg1 <- ggplot() + geom_polygon(data= mex, aes(x=long, y=lat, group=group), fill = NA, color="black") + coord_fixed(1.3)
+gg1 <- ggplot() + geom_polygon(data= mex, aes(x=long, y=lat, group= group), fill = NA, color="black") + coord_fixed(1.3)
 gg1 + geom_point(data= demanda_gas3, aes(x=lon, y= lat, color=demanda_gas3$Petrolifero,  size=ifelse(demanda_gas3$X2017==0, NA, demanda_gas3$X2017), na.rm=TRUE))
 
 
@@ -127,17 +157,67 @@ for (id in 1:nrow(geocodes_aux)){
 }
 
 
-####
 
+#### Base de datos de demanda de gasolinas en el país por centro
 write.csv(demanda_gas3, file="demanda_gasolina_centro.csv", row.names=FALSE)
+
+### Importar información ya trabajada anteriormente
+demanda_gas3 <- read.csv(file="demanda_gasolina_centro.csv")
 demanda_gas3 <- melt(demanda_gas3, id.vars=c("locacion", "Petrolifero", "lon", "lat"), variable.name="ano", value.name="mbd")
 demanda_gas3$ano <- as.numeric(str_sub(demanda_gas3$ano, start=2, end=5))
 
+#Gráfica demanda de 1993 a 2017 de manera descriptiva (scatterplot)
 gg <- ggplot(demanda_gas3, aes(x=ano, y=mbd))
 gg + geom_point(aes(color=Petrolifero)) + facet_wrap(~locacion) + scale_x_continuous(breaks=seq(from=1993, to=2017, by=3)) + theme(axis.text.x = element_text(angle=90))
 
+#Para mapear la información de demanda
+mex <- map_data("worldHires", "Mexico")
+gg1 <- ggplot() + geom_polygon(data= mex, aes(x=long, y=lat, group= group), fill = NA, color="black") + coord_fixed(1.3) 
+map <- gg1 + geom_point(aes(x = lon, y=lat, size = mbd, color=Petrolifero), data=demanda_gas3, alpha = 0.5) + scale_size_continuous(range = c(1,10), breaks = c(1, 5, 10, 30, 50, 80, 100)) + labs(size = 'mbd')
+map <- map + geom_point(aes(x=lon, y=lat, size = value, color=Producto), data=p_producto, alpha = 0.5) + labs(size = 'mbd')
 
-## Cargar la informaci?n de prospectiva
+ini <- tibble(created_at = as.Date('1992-01-01'), mbd=0, lon=-110, lat=25)
+fin <- tibble(created_at = seq(as.Date('2018-01-01'), as.Date('2019-01-01'), by = 'years'), mbd=0, lon=-110, lat=25)
+
+demanda_gas3$ano <- ymd(sprintf("%d-01-01", demanda_gas3$ano))
+
+#animación del mapa
+mapa <- gg1 + geom_point(aes(x = lon, y=lat, size = mbd, color=Petrolifero, frame = ano, cumulative=TRUE), 
+                        data=demanda_gas3, alpha = 0.5) + 
+  geom_point(aes(x=lon, y=lat, size= value, color=Producto, frame = variable, cumulative = TRUE), 
+             data=p_producto, alpha=0.5) +  
+  geom_point(aes(x=lon, y=lat, size=mbd, frame = created_at, cumulative=TRUE),
+             data = ini, alpha = 0) +
+  geom_point(aes(x=lon, y=lat, size=mbd, frame = created_at, cumulative=TRUE),
+             data = fin, alpha = 0) +
+  scale_size_continuous(range = c(1,10), breaks = c(1, 5, 10, 30, 50, 80,100)) + labs(size = 'Demanda/Oferta (mbd)')
+
+
+library(magick)
+##devtools::install_github("nteetor/gganimate") 
+library(animation)
+library(gganimate)
+ani.options(interval = 0.5, ani.width = 1000, ani.height = 1000, ani.res = 1000)
+mapa <- gganimate::gg_animate(mapa, cumulative = TRUE, filename = "output.gif")
+gg_animate_save(mapa, "oferta_demanda_gasolina.gif")
+
+
+#######################################################################
+# Estimar las distancias entre los puntos de demanda y las refinerías #
+#######################################################################
+
+for (i in nrow(capacidad))
+{
+x <- paste0("dist", gsub(" ", "", capacidad[i,1]))
+demanda_gas3[,x] <- distHaversine(cbind(demanda_gas3$lon, demanda_gas3$lat), cbind(capacidad$lon[i], capacidad$lat[i]))
+}
+
+
+
+########################################
+# Cargar la informacion de prospectiva #
+########################################
+
 prospectiva.wide <- read.csv("G:/Proyectos/Prospectiva 2018-2032/Prospectiva/Para SENER/Base prospectiva 2018-2032.csv")
 prospectiva.long <- melt(prospectiva.wide, id=c("tipo", "ronda", "actividad", "activo", "provincia", "ubic", "p_prin_tipo_h", "hidroc_principal", "clasificacion", "licitacion", "bloque", "empresa", "escenario", "concepto") )
 colnames(prospectiva.long)[colnames(prospectiva.long)=="value"] <- "montodiario"
