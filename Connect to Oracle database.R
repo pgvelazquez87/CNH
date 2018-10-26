d0d11c6d4cd874c0ae4d6af50471a79a2975e12c
Pablo Gerardo Velázquez Villa <pablo.velazquez@cnh.gob.mx>
2018-10-25 20:17:49 -0500

    cambios a oracle

--- Connect to Oracle database.R
+++ Connect to Oracle database.R
@@ -4,7 +4,7 @@
 # 25 de octubre de 2018                                                         #
 #################################################################################
 
-
+options(java.parameters = "-Xmx8048m")
 library(RJDBC)
 
 jdbcDriver <- JDBC("oracle.jdbc.OracleDriver",classPath="Z:\\Proyectos\\ojdbc6.jar")
@@ -12,3 +12,20 @@ valid <- dbConnect(jdbcDriver, "jdbc:oracle:thin:@//172.16.120.3:1521/cnih", "cm
 
 balance_gas <- dbReadTable(valid, "BALANCE_GAS")
 head(balance_gas)
+
+produccion_campos <- dbReadTable(valid, "PRODUCCION_CAMPOS")
+head(produccion_campos)
+
+
+library(dplyr)
+library(ggplot2)
+
+produccion_campos$fecha <- as.Date(produccion_campos$FECHA)
+head(produccion_campos)
+
+prodaceite <- as.data.frame(produccion_campos %>% group_by(format(produccion_campos$fecha, "%Y-%m"), CAMPO_PORTAL) %>% summarize(aceite = sum(PETROLEO_MBD)))
+colnames(prodaceite)[1] <- "fecha"
+prodaceite$fecha <- as.Date(as.yearmon(prodaceite$fecha))
+View(prodaceite)
+
+ggplot(data=prodaceite[prodaceite$fecha > as.Date('2000-01-01') & prodaceite$aceite>50, ], aes(x=fecha, y=aceite)) + geom_point(aes(color = CAMPO_PORTAL)) + scale_shape_discrete(name="Campo")
